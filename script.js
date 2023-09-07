84bf512bee8953b378ca360ccb6bc747b978856f
Aleng GL <aleng@webglsoft.com>
2023-09-07 14:41:11 +0800

    modify click event

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let currentInput = "0";
 let previousInput = "";
 let operation = null;
 let memory = roundNumber(parseFloat(localStorage.getItem('memory')), 8) || 0;
+const maxDigits = 13;
 
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculator__keys');
@@ -20,11 +21,24 @@ function roundNumber(number, decimals) {
   return Math.round(number * factor) / factor;
 }
 
+document.addEventListener('keydown', function(event) {
+  if ((event.key >= '0' && event.key <= '9') || event.key === '.') {
+      const button = document.querySelector(`button[data-value="${event.key}"]`);
+      if (button) {
+          button.click();
+      }
+  }
+});
+
 keys.addEventListener('click', (e) => {
   if (!e.target.matches('button')) return;
   const key = e.target;
   const action = key.dataset.action;
   const keyValue = key.textContent;
+
+  if ((currentInput.length >= maxDigits)&& !action) { // 新增這一行來限制位數
+    return;
+  }
 
   switch (action) {
     case 'decimal':
@@ -51,6 +65,9 @@ keys.addEventListener('click', (e) => {
         const num2 = parseFloat(currentInput);
         let result = operators[operation](num1, num2);
         result = roundNumber(result, 8);
+        if (Math.abs(result - 1) < 1e-7) {
+          result = 1;
+        }
         currentInput = result.toString();
         operation = null;
         previousInput = '';
@@ -90,114 +107,23 @@ keys.addEventListener('click', (e) => {
       break;
   }
 
-  display.value = currentInput || '0';
+  display.value = formatNumber(currentInput) || '0';
 });
+
+function formatNumber(number) {
+  const num = parseFloat(number);
+  if (Math.abs(num) >= 1e11) {
+      return num.toExponential(4);
+  } else if (Math.floor(num) != num) {
+      const parts = number.split('.');
+      let integerPart = parseInt(parts[0], 10).toLocaleString('en-US');
+      let decimalPart = parts[1].length > 8 ? parts[1].substring(0, 8) : parts[1];
+      return integerPart + '.' + decimalPart;
+  } else {
+      return num.toLocaleString('en-US');
+  }
+}
 
 function toggleSkin() {
   calculator.classList.toggle('dark');
 }
-
-
-// 這是另一種 使用function寫法 要使用HTML需更改//
-// 下面寫法直觀 對後續維護及可讀性較高//
-
-
-// let currentInput = "0";
-// let previousInput = "";
-// let operation = null;
-// let memory = roundNumber(parseFloat(localStorage.getItem('memory')), 8) || 0;
-// let display = document.getElementById('result');
-
-// const operators = {
-//   add: (a, b) => a + b,
-//   subtract: (a, b) => a - b,
-//   multiply: (a, b) => a * b,
-//   divide: (a, b) => a / b,
-// };
-
-// function appendNumber(number) {
-//   if (currentInput === '0') {
-//     currentInput = number;
-//   } else {
-//     currentInput += number;
-//   }
-//   updateDisplay();
-// }
-
-// function appendDecimal() {
-//   if (!currentInput.includes('.')) {
-//     currentInput += '.';
-//   }
-//   updateDisplay();
-// }
-
-// function setOperation(op) {
-//   if (operation && previousInput) {
-//     const num1 = parseFloat(previousInput);
-//     const num2 = parseFloat(currentInput);
-//     let result = operators[operation](num1, num2);
-//     result = roundNumber(result, 8);
-//     currentInput = result.toString();
-//   }
-//   operation = op;
-//   previousInput = currentInput;
-//   currentInput = '0';
-//   updateDisplay();
-// }
-
-// function calculateResult() {
-//     if (operation && previousInput) {
-//       const num1 = parseFloat(previousInput);
-//       const num2 = parseFloat(currentInput);
-//       let result = operators[operation](num1, num2);
-//       result = roundNumber(result, 8);
-//       currentInput = result.toString();
-//       operation = null;
-//       previousInput = '';
-//     }
-//     updateDisplay();
-// }
-
-// function roundNumber(number, decimals) {
-//     const factor = Math.pow(10, decimals);
-//     return Math.round(number * factor) / factor;
-//   }
-
-// function clearAll() {
-//   currentInput = "0";
-//   previousInput = "";
-//   operation = null;
-//   updateDisplay();
-// }
-
-// function backspace() {
-//   currentInput = currentInput.slice(0, -1);
-//   if (currentInput === '') {
-//     currentInput = '0';
-//   }
-//   updateDisplay();
-// }
-
-// function addToMemory() {
-//   memory += roundNumber(parseFloat(currentInput), 8);
-//   localStorage.setItem('memory', roundNumber(memory, 8));
-// }
-
-// function subtractFromMemory() {
-//   memory -= roundNumber(parseFloat(currentInput), 8);
-//   localStorage.setItem('memory', roundNumber(memory, 8));
-// }
-
-// function recallMemory() {
-//   currentInput = memory.toString();
-//   updateDisplay();
-// }
-
-// function updateDisplay() {
-//   display.value = currentInput;
-// }
-
-// function toggleSkin() {
-//   const calculator = document.querySelector('.calculator');
-//   calculator.classList.toggle('dark');
-// }
